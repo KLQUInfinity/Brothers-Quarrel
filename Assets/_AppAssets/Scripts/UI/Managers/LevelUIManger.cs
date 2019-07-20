348e02721c2172714b5b131fe7b95a5eca1b8212
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2019-07-20 18:24:34 +0200

    bendary
    create gameplay scene just for work on it later
    and modify UIManager
    create the Level UI Mangaer Scripte that extend from UI manager  and will be on the game Play scenes

---
+++ Assets/_AppAssets/Scripts/UI/Managers/LevelUIManger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUIManger : UIManager
+{
+    #region Singleton
+    public static LevelUIManger Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+
+}
---
+++ Assets/_AppAssets/Scripts/UI/Managers/UIManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Slider loadingSlider;
+
+    /// <summary>
+    /// Exit the game
+    /// </summary>
+    public void QuitTheGame()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Load the MainMenu Scene
+    /// </summary>
+    public void BackToMainMenu()
+    {
+        StartCoroutine(LoadAsynchronously("MainMenu"));
+    }
+
+    /// <summary>
+    /// Restart the current Scene
+    /// </summary>
+    public void RestartLevel()
+    {
+        StartCoroutine(LoadAsynchronously(SceneManager.GetActiveScene().name));
+    }
+
+    /// <summary>
+    /// Moving from scene to another
+    /// </summary>
+    /// <param name="scencName">index of the scene you will go to</param>
+    public void LoadLevel(string scencName)
+    {
+        StartCoroutine(LoadAsynchronously(scencName));
+    }
+
+    private IEnumerator LoadAsynchronously(string sceneName)
+    {
+        yield return new WaitForSeconds(1f);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        if (loadingSlider)
+        {
+            while (!operation.isDone)
+            {
+                loadingSlider.value = operation.progress;
+
+                yield return null;
+            }
+        }
+        else
+        {
+            Debug.LogError("No loading Slider Found");
+        }
+    }
+}
