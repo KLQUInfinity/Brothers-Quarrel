598f6669b6a88ac8115d9e2faad1879f9740676f
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2019-07-22 13:36:40 +0200

    bendary
    Add joystick in input manager

---
+++ Assets/_AppAssets/Scripts/InputManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public int index;
+
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal_" + index) !=0)
+        {
+            print("Horizontal_" + index+" pressed");
+        }
+
+        if (Input.GetAxis("Vertical_" + index) != 0)
+        {
+            print("Vertical_" + index + " pressed");
+        }
+
+        if (Input.GetAxis("Fire1_" + index) != 0)
+        {
+            print("Fire1_" + index + " pressed");
+        }
+
+        if (Input.GetAxis("Fire2_" + index) != 0)
+        {
+            print("Fire2_" + index + " pressed");
+        }
+
+        if (Input.GetAxis("Fire3_" + index) != 0)
+        {
+            print("Fire3_" + index + " pressed");
+        }
+
+        if (Input.GetAxis("Jump_" + index) != 0)
+        {
+            print("Jump_" + index + " pressed");
+        }
+    }
+}
