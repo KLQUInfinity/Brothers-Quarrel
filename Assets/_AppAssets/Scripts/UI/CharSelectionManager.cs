8f7d975a64e8c1f0e5eea5fa423559443fcb229f
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2019-08-03 17:59:21 +0200

    bendary
    i made the selection Char menu basics (code and ui)

---
+++ Assets/_AppAssets/Scripts/UI/CharSelectionManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CharSelectionManager : MonoBehaviour
+{
+    [HideInInspector] public bool isActive = false;
+
+    [SerializeField] private UIElement[] selectPanels;
+
+    public Sprite[] CharAvatars;
+
+    private int index = 0;
+    [SerializeField] private bool[] selectFlag;
+    private List<int> startControllers;
+
+    private void Start()
+    {
+        foreach (UIElement i in selectPanels)
+        {
+            i.GetComponent<SelectCharPanel>().SelectionManager = this;
+        }
+
+        selectFlag = new bool[CharAvatars.Length];
+        startControllers = new List<int>();
+    }
+
+
+    private void Update()
+    {
+        if (isActive)
+        {
+            if (Input.GetAxis("Start") != 0)
+            {
+                if (Input.GetKeyDown(KeyCode.Joystick1Button9) && !startControllers.Contains(1))
+                {
+                    startControllers.Add(1);
+                    OpenSelectMenu("Controller 1", 1);
+                }
+                else if (Input.GetKeyDown(KeyCode.Joystick2Button9) && !startControllers.Contains(2))
+                {
+                    startControllers.Add(2);
+                    OpenSelectMenu("Controller 2", 2);
+                }
+                else if (Input.GetKeyDown(KeyCode.Joystick3Button9) && !startControllers.Contains(3))
+                {
+                    startControllers.Add(3);
+                    OpenSelectMenu("Controller 3", 3);
+                }
+                else if (Input.GetKeyDown(KeyCode.Joystick4Button9) && !startControllers.Contains(4))
+                {
+                    startControllers.Add(4);
+                    OpenSelectMenu("Controller 4", 4);
+                }
+            }
+        }
+    }
+
+    private void OpenSelectMenu(string controllerName, int controllerIndex)
+    {
+        if (index < selectPanels.Length)
+        {
+            // Give Controller Name, index and Char avatar according to controller index
+            selectPanels[index].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = controllerName;
+            selectPanels[index].GetComponent<SelectCharPanel>().ControllerIndex = controllerIndex;
+            selectPanels[index].GetComponent<SelectCharPanel>().charImg.sprite = CharAvatars[controllerIndex - 1];
+
+            // Show the Select panel
+            selectPanels[index].SwitchVisibility();
+
+            // Increment the index for the next controller
+            index++;
+        }
+    }
+
+    public void RestSelection()
+    {
+        for (int i = 0; i < index; i++)
+        {
+            selectPanels[i].SwitchVisibility();
+        }
+
+        index = 0;
+    }
+
+    public void Activate(bool check)
+    {
+        isActive = check;
+    }
+
+    /// <summary>
+    /// Select that avatar and make it selected
+    /// </summary>
+    /// <param name="index">the index of the selected avatar</param>
+    /// <returns>Check if the avatar Can be selected or not</returns>
+    public bool SelectAvatar(int index)
+    {
+        if (selectFlag[index])
+        {
+            return false;
+        }
+        else
+        {
+            selectFlag[index] = true;
+            return true;
+        }
+    }
+
+    public void DeselectAvatar(int index)
+    {
+        selectFlag[index] = false;
+    }
+}
---
+++ Assets/_AppAssets/Scripts/UI/SelectCharPanel.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectCharPanel : MonoBehaviour
+{
+    [HideInInspector] public CharSelectionManager SelectionManager;
+    [HideInInspector] public int ControllerIndex;
+
+    public Image charImg;
+
+    private int selectIndex = 0;
+    private bool keyDown = false;
+    private bool keyDown2 = false;
+    private bool keyDown3 = false;
+    private bool canSelect = true;
+
+    private void Update()
+    {
+        if (SelectionManager.isActive && GetComponent<UIElement>().Visible && SelectionManager.CharAvatars.Length > 0)
+        {
+            if (Input.GetAxis("Horizontal_" + ControllerIndex) != 0 && canSelect)
+            {
+                if (!keyDown)
+                {
+                    if (Input.GetAxis("Horizontal_" + ControllerIndex) > 0)
+                    {
+                        selectIndex = (selectIndex + 1) % SelectionManager.CharAvatars.Length;
+                    }
+                    else if (Input.GetAxis("Horizontal_" + ControllerIndex) < 0)
+                    {
+                        selectIndex = (selectIndex - 1 < 0) ? SelectionManager.CharAvatars.Length - 1 : selectIndex - 1;
+                    }
+                    charImg.sprite = SelectionManager.CharAvatars[selectIndex];
+                    keyDown = true;
+                }
+            }
+            else { keyDown = false; }
+
+            // Select Char
+            if (Input.GetAxis("Fire1_" + ControllerIndex) != 0 && canSelect)
+            {
+                print("asdfsdaf");
+                if (!keyDown2)
+                {
+                    canSelect = !SelectionManager.SelectAvatar(selectIndex);
+                    keyDown2 = true;
+                }
+            }
+            else { keyDown2 = false; }
+
+            // Deselect Char
+            if (Input.GetAxis("Jump_" + ControllerIndex) != 0 && !canSelect)
+            {
+                if (!keyDown3)
+                {
+                    SelectionManager.DeselectAvatar(selectIndex);
+                    keyDown3 = true;
+                    canSelect = true;
+                }
+            }
+            else { keyDown3 = false; }
+        }
+    }
+}
